10da1cb676fdfa473b9ab1869c2207bbf1d482e9
Harinath Akkati <117885144+hari2022S@users.noreply.github.com>
2022-12-18 21:35:04 +0530

    Update permutations of array.java

--- Recursion/permutations of array.java
+++ Recursion/permutations of array.java
@@ -1,5 +1,5 @@
 // leet code 49 : https://leetcode.com/problems/permutations/description/
-
+// approch 1:
 class Solution {
     public static void  printPermutations(int[] nums,List<List<Integer>> ans,List<Integer> ds,boolean fre[]){
         if(ds.size()==nums.length){ // base condition 
@@ -24,5 +24,38 @@ class Solution {
        boolean fre[] = new boolean[nums.length]; // for marking element has been taken 
        printPermutations(nums,ans,ds,fre);
        return ans; 
+    }
+}
+
+
+// Best approch 2:
+class Solution{ 
+    public void recuPermute(int index,int[] nums,List<List<Integer>> ans){
+        if(index==num.length){
+            // copy the ds into ans
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0; i<nums.length; i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i = index; i<nums.length; i++){
+            swap(i, index, nums);
+            recuPermute(index+1, nums, ans);
+            swap(i, index, nums);
+        }
+    }
+    
+    public static void swap(int i, int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static List<List<Integer>> permute(int[] nums){
+        List<List<Integer>> ans = new arrayList<>();
+        recuPermute(0, nums, ans);
+        return ans;
     }
 }
