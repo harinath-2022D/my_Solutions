92cdd3d367ed50585369bfaa7c1a49bca1224838
Harinath Akkati <117885144+hari2022S@users.noreply.github.com>
2023-01-18 11:33:02 +0530

    Create Find Minimum in Rotated Sorted Array.java

---
+++ Binary Search/Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,57 @@
+// Example 1
+// Input
+
+// 5
+// 3 4 5 1 2
+// Output
+
+// 1
+// Explanation
+
+// The original array was [1,2,3,4,5] rotated 4 times.
+// Java program to find minimum element in a sorted and
+// rotated array
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Solution
+{
+    static int findMin(int arr[], int low, int high)
+    {
+        //Write your code here
+		if (arr[low] <= arr[high]) {
+            return arr[low];
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid + 1] < arr[mid]) {
+                return arr[mid + 1];
+            } else if (arr[mid - 1] > arr[mid]) {
+                return arr[mid];
+            } else if (arr[low] <= arr[mid]) {
+                // if left side is sorted that means right side is unsorted hence move towards right side
+                low = mid + 1;
+            } else {
+                // if left side is not sorted move towards left side
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+class Main {
+    
+ 
+    // Driver Program
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++)
+        arr[i] = sc.nextInt();
+        System.out.println(Solution.findMin(arr, 0, n - 1));
+    }
+}
