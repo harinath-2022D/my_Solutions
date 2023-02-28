e75b8729276dae74df83198e47afcebdcbd7254d
Harinath Akkati <117885144+hari2022S@users.noreply.github.com>
2023-02-28 14:07:49 +0530

    Create maxConsecutiveOnes.java

---
+++ 2 pointer approch/maxConsecutiveOnes.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+class Solution {
+    static int maxOne(int arr[], int n,int k){
+        //Write your code here
+		int numOfZeroesInCurrWindow = 0;
+		int sp = 0;
+		int ep = 0;
+		int ans = 0;
+
+		while(ep<n){
+			//end point is visited now
+			if(arr[ep] == 0) numOfZeroesInCurrWindow++;
+
+			// -------------------1,1,1,0,0,0,1,1,1,1,0]
+			// shrink until window is Invalid.
+			while(numOfZeroesInCurrWindow > k){
+				if(arr[sp] == 0) numOfZeroesInCurrWindow--;
+				sp++;
+			}
+			//on this line of code, our window will always be valid
+			ans = Math.max(ans,ep-sp+1);
+			ep++;
+		}
+		return ans;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int k= sc.nextInt();
+        int array[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            array[i]= sc.nextInt();
+        }
+        Solution Obj = new Solution();
+        System.out.println(Obj.maxOne(array,n,k));
+    }
+}
