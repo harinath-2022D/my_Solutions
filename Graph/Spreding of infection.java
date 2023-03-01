7987a99924c85ef81a989e4316270af3765749f2
Harinath Akkati <117885144+hari2022S@users.noreply.github.com>
2023-03-01 21:27:20 +0530

    Create Spreding of infection.java

---
+++ Graph/Spreding of infection.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+
+      Edge(int src, int nbr) {
+         this.src = src;
+         this.nbr = nbr;
+      }
+   }
+	static class Pair{
+		int node;
+		int time;
+		Pair(int node,int time){
+			this.node = node;
+			this.time = time;
+		}
+	}
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         graph[v1].add(new Edge(v1, v2));
+         graph[v2].add(new Edge(v2, v1));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+      int t = Integer.parseInt(br.readLine());
+      
+      // write your code here
+	   Queue<Pair> q = new LinkedList<>();
+	   int[] vis = new int[vtces];
+	   q.add(new Pair(src,1));
+	   int cnt = 0;
+
+	   while(q.size() > 0){
+		   Pair cp = q.remove();
+		   int cNode = cp.node;
+
+		   if(cp.time>t) break;
+		   if(vis[cp.node] != 0) continue;
+
+		   vis[cp.node]=1;
+		   cnt++;
+		   
+		   for(Edge e: graph[cp.node]){
+			   if(vis[e.nbr] == 0){
+				   q.add(new Pair(e.nbr,cp.time+1));
+			   }
+		   }
+	   }
+	   System.out.println(cnt);
+   }
+
+}
